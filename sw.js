2449d2d2cff22f070fc0efb6e2453ef85eb6c872
hookmanuk <matt@hookman.co.uk>
2021-10-01 13:16:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,8 +33,7 @@ workbox.routing.registerRoute(
     // Check to see if the request's destination is style for an image
     ({ request }) =>
         request.url.startsWith("https://hookmanstatic.blob.core.windows.net/nftgallery/storage/")
-        || request.url.startsWith("https://cardano-mainnet.blockfrost.io/api")
-        || request.url.startsWith("https://infura-ipfs.io/ipfs/")
+        || request.url.startsWith("https://cardano-mainnet.blockfrost.io/api")       
         || request.url.endsWith(".ipfs.infura-ipfs.io/"),
     // Use a Cache First caching strategy
     new workbox.strategies.CacheFirst({
